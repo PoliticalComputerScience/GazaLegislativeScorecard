4b738689b178780832a13f947680e64eaa60c054
guy shoji <guyshoji@berkeley.edu>
2024-11-20 20:11:41 -0800

    added guys test website file

---
+++ galesc-test-guy/src/components/HomePageBanner.js
@@ -0,0 +1,14 @@
+// src/components/Banner.js
+import React from "react";
+import "./HomePageBanner.css"; 
+
+const HomePageBanner = () => {
+  return (
+    <div className="homepagebanner">
+      <h1>Legislative Scorecard</h1>
+      <p>Your source for tracking legislative voting records</p>
+    </div>
+  );
+};
+
+export default HomePageBanner;
---
+++ galesc-test-guy/src/data/legislators.js
@@ -0,0 +1,39 @@
+// src/data/legislators.js
+const legislators = [
+    {
+      id: 1,
+      name: "John Doe",
+      party: "Democrat",
+      votes: {
+        bill1: "Yes",
+        bill2: "No",
+        bill3: "Yes",
+      },
+      score: "95%",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      party: "Republican",
+      votes: {
+        bill1: "No",
+        bill2: "Yes",
+        bill3: "No",
+      },
+      score: "50%",
+    },
+    {
+      id: 3,
+      name: "Sam Lee",
+      party: "Independent",
+      votes: {
+        bill1: "Yes",
+        bill2: "Yes",
+        bill3: "No",
+      },
+      score: "100%",
+    },
+  ];
+  
+  export default legislators;
+  
