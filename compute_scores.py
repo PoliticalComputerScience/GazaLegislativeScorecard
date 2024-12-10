7d3131d652f434b444af301070c98492cbae60c4
Athan Massouras <amassouras@berkeley.edu>
2024-12-09 16:47:33 -0800

    add new data from 2023

--- compute_scores.py
+++ compute_scores.py
@@ -9,6 +9,14 @@ FOLDER_PATH = 'votesheets'
 dataframes = []
 
 for filename in os.listdir(FOLDER_PATH):
+    if filename.endswith('.xlsx'):
+        file_path = os.path.join(FOLDER_PATH, filename)
+        df = pd.read_excel(file_path)
+        csv_filename = filename.replace('.xlsx', '.csv')
+        csv_file_path = os.path.join(FOLDER_PATH, csv_filename)
+        df.to_csv(csv_file_path, index=False)
+
+for filename in os.listdir(FOLDER_PATH):
     if filename.endswith('.csv'): 
         file_path = os.path.join(FOLDER_PATH, filename)
         df = pd.read_csv(file_path)
@@ -19,38 +27,26 @@ combined_table = pd.concat(dataframes, ignore_index = True)
 
 #perform matrix transformations on df
 def vote_data_transform(data):
-    bill_matrix = data.iloc[:, 2:7].fillna(value=0)
-    vote_matrix = data.iloc[:, 7:107].fillna(value=0)
-
-    print(bill_matrix)
-    print(type(bill_matrix))
-
-    print(vote_matrix)
-    print(type(vote_matrix))
-
+    bill_matrix = data.iloc[:, 2:7].fillna(value=0.)
+    vote_matrix = data.iloc[:, 7:107].fillna(value=0.)
+    
     bill_matrix_transposed = bill_matrix.T
 
     result = np.matmul(bill_matrix_transposed, vote_matrix)
     return result
 
 new_mat = vote_data_transform(combined_table)
-print(new_mat.columns)
-print(new_mat[["Bernie Sanders (I-VT (D-VT))"]])
 
 # Write output to json
 largest_score = new_mat.abs().max().max()
 scores = []
-print(largest_score)
 with open("scores.json", "w") as outfile:
     for senator in new_mat.columns:
         df_entry = new_mat[[senator]]
 
         name, state = senator.split('(', 1)
-        print(name, type(name))
-        print(state, type(state))
 
         weights = list((df_entry.values / largest_score).flatten())
-        print(weights, type(weights))
 
         scores.append({
             "name": name[:-1],
